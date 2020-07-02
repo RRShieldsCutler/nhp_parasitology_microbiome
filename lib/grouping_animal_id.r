2c02c0a0b7220089a94f05989006d0f0fe994c26
RRShieldsCutler <cutler.robin@gmail.com>
2020-07-02 13:09:12 -0500

    Animal-merging

---
+++ lib/grouping_animal_id.r
@@ -0,0 +1,34 @@
+## Aggregate the data in the mapping file by Animal ID
+
+animal.meta <- read.delim('../../data/nhp_per-animal_metadata.txt',
+                     header = T, sep = '\t', check.names = F)
+worm.cts <- read.delim('../../data/nhp_animal_parasite_counts.txt',
+                       header = T, sep = '\t', check.names = F)
+dim(worm.cts)
+worm.mean.counts <- aggregate(worm.cts[,2:12], list(worm.cts$AnimalID), mean)
+hist(worm.mean.counts$all_parasites_ct)
+colnames(worm.mean.counts)[1] <- "AnimalID"
+worm.mean.counts$all_parasites_presence <- worm.mean.counts$all_parasites_ct > 0
+table(worm.mean.counts$all_parasites_presence)
+
+nrow(worm.mean.counts[worm.mean.counts$all_parasites_ct == 0,])
+nrow(worm.mean.counts[worm.mean.counts$all_parasites_ct > 0 & worm.mean.counts$all_parasites_ct <= 10,])
+nrow(worm.mean.counts[worm.mean.counts$all_parasites_ct > 10,])
+
+worm.mean.counts$all_parasites_presence[worm.mean.counts$all_parasites_presence==TRUE] <- "positive"
+worm.mean.counts$all_parasites_presence[worm.mean.counts$all_parasites_presence==FALSE] <- "negative"
+
+worm.mean.counts$all_parasites_semiquant <- cut(worm.mean.counts$all_parasites_ct,
+                                                breaks = c(-Inf,0,10,Inf), right=T,
+                                                labels = c("none","low","high"))
+table(worm.mean.counts$all_parasites_semiquant)
+
+nhp.parasite.meta <- merge(worm.mean.counts, animal.meta, by='AnimalID')
+
+write.table(nhp.parasite.meta, file = '../../data/nhp_animal_parasite_map_200602.txt', quote = F, sep = '\t', row.names = F)
+# make the howler and douc ones
+
+howler.parasite.meta <- nhp.parasite.meta[nhp.parasite.meta$SpeciesCommonName == "mantled-howling-monkey",]
+douc.parasite.meta <- nhp.parasite.meta[nhp.parasite.meta$SpeciesCommonName == "red-shanked-douc",]
+write.table(howler.parasite.meta, file = '../../data/howler_animal_parasite_map_200602.txt', quote = F, sep = '\t', row.names = F)
+write.table(douc.parasite.meta, file = '../../data/douc_animal_parasite_map_200602.txt', quote = F, sep = '\t', row.names = F)
